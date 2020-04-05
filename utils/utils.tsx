b158b554f23cfe42d93a8bb20844e76a9ffdd73d
Ones <964981348@qq.com>
2020-04-05 12:23:16 +0800

    Update utils.tsx

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -123,3 +123,13 @@ export const createFormData = (formDatas, file) => {
   }
   return formData;
 };
+
+// 文件转base64,图片用
+export const getBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+};
