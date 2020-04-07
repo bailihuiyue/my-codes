348340243f076613d2a357d0d275917220c16b1a
Ones <964981348@qq.com>
2020-04-07 15:11:26 +0800

    Update utils.tsx

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,11 +1,11 @@
-import { parse } from 'querystring';
+import { parse } from "querystring";
 
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
 
 export const isUrl = (path: string): boolean => reg.test(path);
 
-export const getPageQuery = () => parse(window.location.href.split('?')[1]);
+export const getPageQuery = () => parse(window.location.href.split("?")[1]);
 
 // 判断是否是正确的手机串号
 export const isIMEI = (imei: string) => {
@@ -18,23 +18,17 @@ export const isIMEI = (imei: string) => {
   let mul = 2;
   let l = 14;
   for (let i = 0; i < l; i++) {
-
     let digit = imei.substring(l - i - 1, l - i);
     let tp = parseInt(digit, 10) * mul;
-    if (tp >= 10)
-      sum += (tp % 10) + 1;
-    else
-      sum += tp;
-    if (mul == 1)
-      mul++;
-    else
-      mul--;
+    if (tp >= 10) sum += (tp % 10) + 1;
+    else sum += tp;
+    if (mul == 1) mul++;
+    else mul--;
   }
-  let chk = ((10 - (sum % 10)) % 10);
-  if (chk != parseInt(imei.substring(14, 15), 10))
-    return false;
+  let chk = (10 - (sum % 10)) % 10;
+  if (chk != parseInt(imei.substring(14, 15), 10)) return false;
   return true;
-}
+};
 
 // 下载后台传过来的流文件(兼容ie11)
 export const download = (response, fileName) => {
@@ -42,7 +36,7 @@ export const download = (response, fileName) => {
     if (window.navigator.msSaveOrOpenBlob) {
       navigator.msSaveBlob(blob, fileName);
     } else {
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       const body = document.getElementsByTagName("body")[0];
       link.href = window.URL.createObjectURL(blob);
       link.download = fileName;
@@ -52,8 +46,8 @@ export const download = (response, fileName) => {
       body.removeChild(link);
     }
     return true;
-  })
-}
+  });
+};
 
 // vue 图片查看器:v-viewer,示例项目:Invoice Checking Queue
 
@@ -61,56 +55,62 @@ export const download = (response, fileName) => {
 // main.js 添加如下方法
 const IE11RouterFix = {
   methods: {
-    hashChangeHandler: function() {
+    hashChangeHandler: function () {
       this.$router.push(
         window.location.hash.substring(1, window.location.hash.length)
       );
     },
-    isIE11: function() {
+    isIE11: function () {
       return !!window.MSInputMethodContext && !!document.documentMode;
-    }
+    },
   },
-  mounted: function() {
+  mounted: function () {
     if (this.isIE11()) {
       window.addEventListener("hashchange", this.hashChangeHandler);
     }
   },
-  destroyed: function() {
+  destroyed: function () {
     if (this.isIE11()) {
       window.removeEventListener("hashchange", this.hashChangeHandler);
     }
-  }
+  },
 };
 
 new Vue({
   router,
   store,
   i18n,
-  render: h => h(App),
+  render: (h) => h(App),
   // 同时添加这行
-  mixins: [IE11RouterFix]
+  mixins: [IE11RouterFix],
 }).$mount("#app");
-
 
 // 时间格式化为03/18/2020 13：34这种 MM/DD/YY hh:mm
 export const formatDate = (date: Date) => {
   const YY = date.getFullYear();
-  const MM = `${date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1}`;
-  const DD = (date.getDate() < 10 ? `0${date.getDate()}` : date.getDate());
-  const hh = `${date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()}`;
-  const mm = `${date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()}`;
+  const MM = `${
+    date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+  }`;
+  const DD = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+  const hh = `${
+    date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+  }`;
+  const mm = `${
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+  }`;
   // const ss = (date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds());
   return `${MM}/${DD}/${YY} ${hh}:${mm}`;
-}
-
+};
 
 // 前端解决缓存问题,写在模板html中即可
-{/* <script>
+{
+  /* <script>
   if(!window.sessionStorage.getItem("hasReload")){
     window.sessionStorage.setItem("hasReload","true");
     window.location.reload(true); 
   }
-</script> */}
+</script> */
+}
 
 // 创建formData,同时上传表单和文件,支持多个文件
 export const createFormData = (formDatas, file) => {
@@ -136,4 +136,15 @@ export const getBase64 = (file) => {
     reader.onload = () => resolve(reader.result);
     reader.onerror = (error) => reject(error);
   });
+};
+
+// 删除object中的null值
+export const removeNullData = (data) => {
+  let obj = {};
+  for (let k in data) {
+    if (data[k] !== null) {
+      obj[k] = data[k];
+    }
+  }
+  return obj;
 };
